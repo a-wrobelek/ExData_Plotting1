eeeacdf292f07b8b00c7aea273bcc3784c9c44c0
a-wrobelek <a_wrobelek@yahoo.com>
2016-04-25 16:22:16 -0400

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,22 @@
+setwd() ## set directory for wherever the file "household_power_consumption.txt" is stored
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+str(data)
+data2 <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+png("plot4.png", width = 480, height = 480)
+par(mfrow = c(2,2))
+x <- strptime(paste(data2$Date, data2$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+y1 <- as.numeric(data2$Global_active_power)
+y2 <- as.numeric(data2$Voltage)
+y3 <- as.numeric(data2$Sub_metering_1)
+y4 <- as.numeric(data2$Sub_metering_2)
+y5 <- as.numeric(data2$Sub_metering_3)
+y6 <- as.numeric(data2$Global_reactive_power)
+plot(x, y1, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(x, y2, type ="l", xlab = "datetime", ylab = "Voltage")
+plot(x, y3, type="l", xlab="", ylab="Energy sub metering")
+lines(x, y4, type="l", col = "red")
+lines(x, y5, type="l", col = "blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+plot(x, y6, type ="l", xlab = "datetime", ylab = "Global_reactive_power")
+dev.off()
